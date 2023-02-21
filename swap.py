d03a9ac8a99875d7b7e7c7308a7e652492db40c8
Shraddha Shrivastav <shraddha.shrivastav@alfalaval.com>
2023-02-21 18:04:55 +0530

    test commit

--- swap.py
+++ swap.py
@@ -1,2 +1,7 @@
 # exhange the value of 2 numbers without using third variable
 first_number = int(input("input first number : "))
+second_number = int(input("input second number : "))
+first_number = first_number + second_number
+second_number = first_number - second_number
+first_number = first_number - second_number
+print("\nafter swapping first_number is :", first_number, "\n second_number is :", second_number)
