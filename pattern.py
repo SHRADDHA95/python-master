dfacc08cb17d141a5b3e29ca6e2a68dcaadb1fdb
Shraddha Shrivastav <shraddha.shrivastav@alfalaval.com>
2023-02-23 21:54:40 +0530

    python multiplication table using range function

--- pattern.py
+++ pattern.py
@@ -1,1 +1,7 @@
 # pattern 10 +1010+101010
+in_number = int(input("input number : "))
+temp1 = str(in_number)
+temp2 = temp1 + temp1
+temp3 = temp1 + temp1 + temp1
+total = in_number + int(temp2) + int(temp3)
+print("total sum is : ", total)
